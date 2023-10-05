c0c6fd901bdab75e222a4b5f90b4afe3fc9334f3
ipz233_vvv <jumpergooog@gmail.com>
2023-10-05 11:23:30 +0300

    removed architectury as dependency

--- forge/src/main/java/io/github/jumperonjava/customcursorcomm/forge/CustomcursorcommForge.java
+++ forge/src/main/java/io/github/jumperonjava/customcursorcomm/forge/CustomcursorcommForge.java
@@ -1,6 +1,5 @@
 package io.github.jumperonjava.customcursorcomm.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import io.github.jumperonjava.customcursorcomm.CustomCursorInit;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -9,7 +8,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class CustomcursorcommForge {
     public CustomcursorcommForge() {
 		// Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(CustomCursorInit.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
+        //EventBuses.registerModEventBus(CustomCursorInit.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
             CustomCursorInit.init();
     }
 }
