4f758c8898a3269d67e11f2d718ed2735b435764
ipz233_vvv <jumpergooog@gmail.com>
2023-10-05 11:33:09 +0300

    change default size

--- common/src/main/java/io/github/jumperonjava/customcursorcomm/cursor/CursorSettings.java
+++ common/src/main/java/io/github/jumperonjava/customcursorcomm/cursor/CursorSettings.java
@@ -7,7 +7,7 @@ import net.minecraft.util.Identifier;
 public class CursorSettings implements Cloneable{
     public float x=1f;
     public float y;
-    public int size=16;
+    public int size=32;
     public boolean enabled=false;
     public Identifier identifier = PLACEHOLDER_CURSOR;
     public final static Identifier PLACEHOLDER_CURSOR = new Identifier("textures/item/diamond_sword.png");
