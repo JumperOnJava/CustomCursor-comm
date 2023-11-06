ae1168d7dff9b3fe20a5fdbaf69e9ad56ba2b257
ipz233_vvv <jumpergooog@gmail.com>
2023-11-06 17:14:37 +0200

    fixed color

--- common/src/main/java/io/github/jumperonjava/customcursorcomm/cursor/CursorEditScreen.java
+++ common/src/main/java/io/github/jumperonjava/customcursorcomm/cursor/CursorEditScreen.java
@@ -153,54 +153,8 @@ public class CursorEditScreen extends Screen {
                 0,
                 0,
                 0,144,144,144,144);
-        RenderSystem.setShaderColor(255,255,255,255);
-
+        RenderSystem.setShaderColor(1,1,1,1);
         context.getMatrices().pop();
         context.disableScissor();
-    }
-    public static int hsvToRgb(float hue, float saturation, float value) {
-        float r, g, b;
-
-        int h = (int)(hue * 6);
-        float f = hue * 6 - h;
-        float p = value * (1 - saturation);
-        float q = value * (1 - f * saturation);
-        float t = value * (1 - (1 - f) * saturation);
-
-        if (h == 0) {
-            r = value;
-            g = t;
-            b = p;
-        } else if (h == 1) {
-            r = q;
-            g = value;
-            b = p;
-        } else if (h == 2) {
-            r = p;
-            g = value;
-            b = t;
-        } else if (h == 3) {
-            r = p;
-            g = q;
-            b = value;
-        } else if (h == 4) {
-            r = t;
-            g = p;
-            b = value;
-        } else if (h <= 6) {
-            r = value;
-            g = p;
-            b = q;
-        } else {
-            throw new RuntimeException("Something went wrong when converting from HSV to RGB. Input was " + hue + ", " + saturation + ", " + value);
-        }
-
-        r*=r;
-        r*=r;
-        g*=g;
-        g*=g;
-        b*=b;
-        b*=b;
-        return ColorHelper.Argb.getArgb(255,(int)(r * 255),(int)(g * 255),(int)(b * 255));
     }
 }
